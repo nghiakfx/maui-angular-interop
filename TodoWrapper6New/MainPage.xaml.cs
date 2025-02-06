72a163a74e2b7d127e4876916c1bd10835190579
Nghia Dai Le <nghia.kofax@gmail.com>
2025-02-06 17:28:00 +0700

    Test with https://localhost:4433

--- TodoWrapper6New/MainPage.xaml.cs
+++ TodoWrapper6New/MainPage.xaml.cs
@@ -2,9 +2,22 @@
 
 public partial class MainPage : ContentPage
 {
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+
+    private void Bwv_BlazorWebViewInitialized(object sender, Microsoft.AspNetCore.Components.WebView.BlazorWebViewInitializedEventArgs e)
+    {
+        // change the 'localhost' to '10.0.2.2'
+        //e.WebView.LoadRequest(new NSUrlRequest(new NSUrl("https://10.0.2.2:5001/")));
+
+
+#if ANDROID
+        //e.WebView.Settings.MixedContentMode = Android.Webkit.MixedContentHandling.AlwaysAllow;
+#else
+        //e.WebView.Configuration.Preferences.SetValueForKey(NSNumber.FromBoolean(true), new NSString("allowFileAccessFromFileURLs"));
+#endif
+    }
 }
 
