4582052c3aae93e8870dd5856fcd0ce86427c8eb
Nghia Dai Le <nghia.kofax@gmail.com>
2025-02-06 17:52:18 +0700

    Test with: https://10.0.2.2

--- TodoWrapper6New/MainPage.xaml.cs
+++ TodoWrapper6New/MainPage.xaml.cs
@@ -5,6 +5,20 @@ public partial class MainPage : ContentPage
     public MainPage()
     {
         InitializeComponent();
+
+        var handler = new HttpClientHandler();
+
+#if DEBUG
+        handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) =>
+        {
+            if (cert != null && cert.Issuer.Equals("CN=localhost"))
+                return true;
+            return errors == System.Net.Security.SslPolicyErrors.None;
+        };
+#endif
+
+        var client = new HttpClient(handler);
+
     }
 
     private void Bwv_BlazorWebViewInitialized(object sender, Microsoft.AspNetCore.Components.WebView.BlazorWebViewInitializedEventArgs e)
